28077bbaf120a27ff6b4021271e01c1aa62d0514
Akash Katakam <katakam.a@husky.neu.edu>
2019-07-21 16:04:39 -0400

    Installing watch and adding cloudwatch config file

--- instance_setup.sh
+++ instance_setup.sh
@@ -49,3 +49,30 @@ sudo ./install auto
 # If code deploy aget is not running then start the service first
 # sudo service codedeploy-agent start
 sudo service codedeploy-agent status
+wget https://s3.us-east-1.amazonaws.com/amazoncloudwatch-agent-us-east-1/centos/amd64/latest/amazon-cloudwatch-agent.rpm
+sudo rpm -U ./amazon-cloudwatch-agent.rpm
+cd /opt
+sudo mkdir cloudwatch
+cat << EOF | sudo tee -a /opt/cloudwatch/cloudwatch-config.json
+{
+    "agent": {
+        "metrics_collection_interval": 10,
+        "logfile": "/var/logs/amazon-cloudwatch-agent.log"
+    },
+    "logs": {
+        "logs_collected": {
+            "files": {
+                "collect_list": [
+                    {
+                        "file_path": "/opt/tomcat/logs/csye6225.log",
+                        "log_group_name": "csye6225_su2019",
+                        "log_stream_name": "webapp",
+                    }
+                ]
+            }
+        },
+        "log_stream_name": "cloudwatch_log_stream"
+    }
+}
+EOF
+cd /home/centos
